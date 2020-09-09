64a0fe14caa1466298224624444c955d9a19b914
esauerbo <70536670+esauerbo@users.noreply.github.com>
2020-09-09 15:08:39 -0700

    Add files via upload

---
+++ schools.js
@@ -0,0 +1,62 @@
+var schools =
+[{
+        "name": " Korea Advanced Institute of Science & Technology",
+        "coordinates": [36.3697191, 127.362537001151]
+    }, {
+        "name": " Weizmann Institute of Science Israel",
+        "coordinates": [31.9078499, 34.8133409244421]
+    }, {"name": " Amkor Technology", "coordinates": [14.43879045, 121.048057857141]}, {
+        "name": " UC Davis",
+        "coordinates": [38.52247515, -121.751392674913]
+    }, {
+        "name": " University of California: Riverside",
+        "coordinates": [33.97127125, -117.337787518449]
+    }, {
+        "name": " University of Illinois at Urbana-Champaign",
+        "coordinates": [40.101976, -88.2314378]
+    }, {
+        "name": " Technion - Israel Institute of Technology",
+        "coordinates": [32.7767536, 35.0241452903301]
+    }, {
+        "name": " Stanford University",
+        "coordinates": [37.43131385, -122.169365354983]
+    }, {
+        "name": " University of California: Los Angeles",
+        "coordinates": [34.07088865, -118.446731966638]
+    }, {
+        "name": " University of Queensland",
+        "coordinates": [-27.4975237, 153.0120773]
+    }, {
+        "name": " Lawrence Livermore National Laboratory",
+        "coordinates": [37.687781, -121.708309338646]
+    }, {
+        "name": " Florida State University",
+        "coordinates": [30.44235995, -84.2974786716626]
+    }, {
+        "name": " University of California: San Diego",
+        "coordinates": [32.87935255, -117.231100493855]
+    }, {"name": " NASA Ames Research Center", "coordinates": [37.4196992, -122.0634915]}, {
+        "name": " TRIUMF",
+        "coordinates": [42.1403726, 24.793830987713]
+    }, {
+        "name": " University of Surrey",
+        "coordinates": [51.24303255, -0.590013824660236]
+    }, {
+        "name": " Purdue University",
+        "coordinates": [40.4275052, -86.9122769]
+    }, {
+        "name": " Technische Universitat Berlin",
+        "coordinates": [52.51101585, 13.3269541409597]
+    }, {
+        "name": " Boston University",
+        "coordinates": [42.35050035, -71.1025599049017]
+    }, {
+        "name": " Northern Illinois University",
+        "coordinates": [41.9370934, -88.7755748216438]
+    }, {"name": " Singapore Management University", "coordinates": [1.296126, 103.8496233]}, {
+        "name": " UC Berkeley",
+        "coordinates": [37.8753497, -122.239633649188]
+    }, {
+        "name": " Universita di Salerno",
+        "coordinates": [40.7720081, 14.7911797474273]
+    }, {"name": " The University of North Carolina at Charlotte", "coordinates": [35.3111064, -80.7310213460728]}];
